509c3afa1e53be7674684abfba4c406232510dd9
Mohammed Nadeem <mohammed.nadeem442@gmail.com>
2018-04-12 15:57:49 +0530

    Added Extra Folders

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var express=require('express');
-var http=require('http');
-var app=express();
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
+var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
-var port=process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 1337;;
-var ip= process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var port = process.env.PORT || 1337;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 1337;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get('/',function(request,response){
-    response.send('Hi Nadeem');
-});
-app.listen(port,ip,function(){
-    console.log('Port Number : '+port + 'Server Ip : '+ ip);
-});
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
+setupController(app);
+apiController(app);
+
+app.listen(port,ip);
